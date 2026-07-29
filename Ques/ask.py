d2e2266210d5f68b13c483de09f2a8e1782b575b
mispython <mis.saspython@gmail.com>
2026-07-29 16:20:23 +0800

    Update ask.py

--- Ques/ask.py
+++ Ques/ask.py
@@ -1,15 +1,28 @@
-for refernece
+PATTERNS = [
+    # YYYYMMDD (added)
+    r"(?P<prefix>[a-zA-Z]*)(?P<year>\d{4})(?P<mm>\d{2})(?P<dd>\d{2})",
+    # Existing patterns
+    r"(?P<prefix>[a-zA-Z]*)(?P<yy>\d{2})(?P<mm>\d{2})(?P<dd>\d{2})",
+    # ... others ...
+]
 
-/* FCYCA FCYFD */
-DATA FCYCA FCYFD;
-   	SET DP_FCY.FCY&REPTMON&NOWK&REPTYY 
-        IDP_FCY.IFCY&REPTMON&NOWK&REPTYY;
+def extract_key(filename: str):
+    for pattern in PATTERNS:
+        m = re.search(pattern, filename)
+        if not m:
+            continue
+        gd = m.groupdict()
 
-	CURBAL=CURBALRM;
-   	IF CUSTCODE IN (77,78,95,96);
+        if 'year' in gd:
+            year = int(gd['year'])
+        else:
+            yy = int(gd.get('yy') or 0)
+            year = 2000 + yy if yy < 100 else yy
 
-   	IF 3000000000<=ACCTNO<=3999999999 THEN OUTPUT FCYCA;
-   	ELSE 								   OUTPUT FCYFD; 
+        mm = int(gd.get('mm') or 0)
+        dd = int(gd.get('dd') or 0)
+        ww = int(gd.get('ww') or 0)
 
-	KEEP ACCTNO CURBAL USER3 PRODUCT;
-RUN;
+        return (year, mm, ww, dd)
+
+    return None
