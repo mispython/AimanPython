494f7daf99218b8495d4747bf458dec4c60cbb5f
mispython <mis.saspython@gmail.com>
2026-07-29 16:24:04 +0800

    Update ask.py

--- Ques/ask.py
+++ Ques/ask.py
@@ -1,20 +1,14 @@
-PATTERNS = [
-    # YYYYMMDD (added)
-    r"(?P<prefix>[a-zA-Z]*)(?P<year>\d{4})(?P<mm>\d{2})(?P<dd>\d{2})",
-    # Existing patterns
-    r"(?P<prefix>[a-zA-Z]*)(?P<yy>\d{2})(?P<mm>\d{2})(?P<dd>\d{2})",
-    # ... others ...
-]
-
 def extract_key(filename: str):
     for pattern in PATTERNS:
         m = re.search(pattern, filename)
         if not m:
             continue
+
         gd = m.groupdict()
 
-        if 'year' in gd:
-            year = int(gd['year'])
+        # Try 4-digit year first
+        if 'yyyy' in gd and gd['yyyy']:
+            year = int(gd['yyyy'])
         else:
             yy = int(gd.get('yy') or 0)
             year = 2000 + yy if yy < 100 else yy
